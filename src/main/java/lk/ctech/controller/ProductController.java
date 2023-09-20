2c746d2c700ed527665bf915757ea1394a637018
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-09-20 13:31:33 +0530

    finalize add new product feature

--- src/main/java/lk/ctech/controller/ProductController.java
+++ src/main/java/lk/ctech/controller/ProductController.java
@@ -5,6 +5,7 @@ import lk.ctech.entity.Product;
 import lk.ctech.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,6 +20,7 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 
+	@PreAuthorize("hasRole('Admin')")
 	@PostMapping(path = "/add", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
 	public Product addNewProduct(@RequestPart("product") Product product,
 								 @RequestPart("imageFile") MultipartFile[] file) {
