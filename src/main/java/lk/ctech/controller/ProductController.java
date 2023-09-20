27c1cef5bc8db366801e6dbee23705a186c99d7a
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-09-20 14:07:40 +0530

    apply get all products feature

--- src/main/java/lk/ctech/controller/ProductController.java
+++ src/main/java/lk/ctech/controller/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @RestController
@@ -47,5 +48,10 @@ public class ProductController {
 			imageModels.add(imageModel);
 		}
 		return imageModels;
+	}
+
+	@GetMapping(path = "/get")
+	public List<Product> getAllProducts(){
+		return productService.getAllProducts();
 	}
 }
--- src/main/java/lk/ctech/service/ProductService.java
+++ src/main/java/lk/ctech/service/ProductService.java
@@ -3,7 +3,11 @@ package lk.ctech.service;
 import lk.ctech.dao.ProductDao;
 import lk.ctech.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -13,5 +17,9 @@ public class ProductService {
 
 	public Product addNewProduct(Product product) {
 		return productDao.save(product);		
+	}
+
+	public List<Product> getAllProducts(){
+			return (List<Product>) productDao.findAll();
 	}
 }
