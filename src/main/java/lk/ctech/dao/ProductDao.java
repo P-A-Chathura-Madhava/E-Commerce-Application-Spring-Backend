71a721f449bbea890468e46ae8af29ffd102fd2a
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-09-22 15:43:08 +0530

    apply pagination feature

--- src/main/java/lk/ctech/dao/ProductDao.java
+++ src/main/java/lk/ctech/dao/ProductDao.java
@@ -1,9 +1,14 @@
 package lk.ctech.dao;
 
 import lk.ctech.entity.Product;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductDao extends CrudRepository<Product, Integer>{
+
+    public List<Product> findAll(Pageable pageable);
 }
--- src/main/java/lk/ctech/service/ProductService.java
+++ src/main/java/lk/ctech/service/ProductService.java
@@ -3,6 +3,8 @@ package lk.ctech.service;
 import lk.ctech.dao.ProductDao;
 import lk.ctech.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -18,8 +20,9 @@ public class ProductService {
         return productDao.save(product);
     }
 
-    public List<Product> getAllProducts() {
-        return (List<Product>) productDao.findAll();
+    public List<Product> getAllProducts(int pageNumber) {
+        Pageable pageable = PageRequest.of(pageNumber, 12);
+        return (List<Product>) productDao.findAll(pageable);
     }
 
     public Product getProductDetailsById(Integer productId) {
